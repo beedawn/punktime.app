094e78d4beedfc1bd5f0a850dce6be0740013319
Bee <bee@bees-mbp.lan>
2023-12-19 19:24:13 -0500

    updates

--- punktimel/src/main.rs
+++ punktimel/src/main.rs
@@ -1,7 +1,7 @@
 use leptos::*;
 
 fn main() {
-   mount_to_body(|| view!{ <p>"Hello, world!" </p> <App /> });
+   mount_to_body(|| view!{ <App /> });
 
 
 
@@ -13,19 +13,53 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
 
 
-
+  let (auth, set_auth) = create_signal(false);
     view!{
-    <button 
-        on:click=move |_| {
-            set_count(3);
-        }
->"Click me: "
-        {move || count.get()}
-        </button>
-    }
+ <Show
+    when=move || auth.get() == false
+    fallback=|| view! {  <LoggedIn />}
+  >
+   <div style="position:absolute;
+    top:40%;
+    left:42%;
+
+      "><Login auth=auth set_auth=set_auth />
+ </div>
+  </Show>
+            }
 
 
+}
+
+
+
+#[component]
+fn Login(auth:ReadSignal<bool>, set_auth:WriteSignal<bool>) -> impl IntoView {
+let (name, set_name) = create_signal("Controlled".to_string());
+let (pw, set_pw) = create_signal("spaghetti2".to_string());
+view!{
+   <div>Username:  <input type="text" on:input=move |ev| { set_name(event_target_value(&ev));} prop:value=name /></div>
+    
+   <div>Password:  <input type="text" on:input=move |ev| {set_pw(event_target_value(&ev));} prop:value=pw /></div>
+    
+    <button on:click= move |_| {
+
+  set_auth(!auth.get());
+
+  //send credentials to server for validation
+
+
+  }> Login </button>
+}
+
+}
+
+
+
+
+#[component]
+fn LoggedIn()-> impl IntoView{
+view!{"Logged in"}
 }
